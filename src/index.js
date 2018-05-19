58f4370344102e6811bb02bd56458ca9d5a7651a
wcg <304827460@qq.com>
2018-05-19 15:10:27 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './favicon.jpg';
+import printMe from './print.js';
 
 function component() {
 	var element = document.createElement('div');
+	var btn = document.createElement('button');
 	
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
 	
-	//将图像添加到现有的div
-	var icon = new Image();
-	icon.src = Icon;
-	element.appendChild(icon);
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+	
+	element.appendChild(btn);
 	
 	return element;
 }
